22e43d64abd991a3cd65be9dd573c3d6c03462f7
auztin410 <auztin410@gmail.com>
2018-06-19 15:42:16 -0400

    Adding logic for multiple pages

--- logic.js
+++ logic.js
@@ -1,13 +1,46 @@
-// $("#bluepill").on("click", function() {
-//     game will start!
-// })
+$("#bluepill").on("click", function() {
+    $("#main").html("<h2> Who is the chosen one? </h2> <p id='correct'>Neyo</p> <p id='wrong'>Morpheus</p> <p id='wrong'>Agent Smith</p> <p id='wrong'>Bob</p>" )
+})
 
-// $("#redpill").on("click", function() {
-//     game will close out.
-// })
+$("#redpill").on("click", function() {
+    $("#main").html("<h2>You have chosen poorly</h2> <p>Go back into the real world you noob.</p>")
+})
 
-setTimeout(firstScreen, 5000);
+// setTimeout(secondScreen, 30000);
 
 function firstScreen() {
-$("#main").html("Test")
+$("#main").html("") 
+}
+
+function secondScreen() {
+    $("#main").html("")
+}
+
+function thirdScreen() {
+    $("#main").html("")
+}
+
+function fourthScreen() {
+    $("#main").html("")
+}
+
+function fifthScreen() {
+    $("#main").html("")
+}
+
+function sixthScreen() {
+    $("#main").html("")
+}
+function seventhScreen() {
+    $("#main").html("")
+}
+function eigthScreen() {
+    $("#main").html("")
+}
+function ninthScreen() {
+    $("#main").html("")
+}
+
+function tenthScreen() {
+    $("#main").html("")
 }
