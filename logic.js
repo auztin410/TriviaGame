cae1e9d0641d5b5ef83ba0b25ac8c9bff7ecddfd
auztin410 <auztin410@gmail.com>
2018-06-19 09:53:52 -0400

    Added some test logic

---
+++ logic.js
@@ -0,0 +1,13 @@
+// $("#bluepill").on("click", function() {
+//     game will start!
+// })
+
+// $("#redpill").on("click", function() {
+//     game will close out.
+// })
+
+setTimeout(firstScreen, 5000);
+
+function firstScreen() {
+$("#main").html("Test")
+}
